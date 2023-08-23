f2f280af9c944c325f034328ec96441de3b493f8
hamza jazyri <hamzajazyri@gmail.com>
2023-08-23 15:41:55 +0100

    implements heading editor

--- src/app/certification-editor/content-elements/heading-element/heading-element.component.ts
+++ src/app/certification-editor/content-elements/heading-element/heading-element.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Editor, NgxEditorModule, Toolbar } from 'ngx-editor';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heading-element',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxEditorModule],
   templateUrl: './heading-element.component.html',
   styleUrls: ['./heading-element.component.scss']
 })
 export class HeadingElementComponent {
+
+  @Input() data: any;
+
+  editor!: Editor;
+  editorContent = new FormControl({ value: '<h1>HEADING TEXT</h1>', disabled: false });
+  toolbar = headingToolbarEditorDefaultConfig;
+
+  ngOnInit(): void {
+    this.editor = new Editor({
+      history: true,
+      keyboardShortcuts: true,
+      inputRules: true,
+    });
+  }
+
+
 }
+
+export const headingToolbarEditorDefaultConfig: Toolbar = [
+  ['italic', 'underline',  'text_color'],
+  // [],
+  // ['code', ],
+  // [],
+  [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+  // ['link', 'image'],
+  // [],
+  ['align_left', 'align_center', 'align_right'],
+  // ['horizontal_rule', 'format_clear']
+];
