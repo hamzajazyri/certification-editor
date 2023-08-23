6a92301dcd915f5ba25e069d339b86e89d6ee83c
hamza jazyri <hamzajazyri@gmail.com>
2023-08-23 18:52:24 +0100

    add table control

--- src/app/certification-editor/content-elements/table-element/table-element.component.ts
+++ src/app/certification-editor/content-elements/table-element/table-element.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table-element',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './table-element.component.html',
   styleUrls: ['./table-element.component.scss']
 })
 export class TableElementComponent {
+  @Input() data!: any;
 
+  values: Array<string> = []
+  tableSize = new FormControl<string>('2x2');
+
+  constructor() {
+    for(let i=1; i<20; i++) {
+      for(let j=1; j<20; j++) {
+        this.values.push(`${i}x${j}`)
+      }
+    }
+  }
+
+
+  get rowsCount(){
+    return new Array(parseInt(this.tableSize.value!.split('x')[0]));
+  }
+  get colsCount(){
+    return new Array(parseInt(this.tableSize.value!.split('x')[1]));
+  }
 }
